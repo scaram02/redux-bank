a3781d75c373b5b2668078fd49c595a8d76f7c3d
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-06-05 20:00:45 -0500

    fix issue

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function App() {
   return (
     <div className="App">
       <h1>{account}</h1>
-      <h2>tutorial from https://www.youtube.com/watch?v=9jULHSe41ls</h2>
+      <p>https://www.youtube.com/watch?v=9jULHSe41ls Redux tutorial</p>
       <button onClick={() => depositMoney(1000)}>Deposit</button>
       <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -2,15 +2,19 @@
 // for bigger apps can of course do multiple files
 
 export const depositMoney = amount => {
-    return (dispatch) => ({
+    return (dispatch) => {
+        dispatch({
         type: "deposit",
         payload: amount
     })
 }
+}
 
 export const withdrawMoney = amount => {
-    return (dispatch) => ({
+    return (dispatch) => {
+        dispatch({
         type: "withdraw", 
         payload: amount
     })
+}
 }
